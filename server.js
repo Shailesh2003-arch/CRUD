9cf855a84f0ed2b33526bc32dda3f8d5b576997f
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-05-27 22:18:55 +0530

    add post request

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
+
+const teaData = [];
+let id = 1;
+
+app.post("/teas", (req, res) => {
+  const { name, price } = req.body;
+  const newTea = {
+    id: id++,
+    name,
+    price,
+  };
+  teaData.push(newTea);
+  res.status(201).send(newTea);
+});
 
 app.get("/teas", (req, res) => {
-  res.status(200).send(`Request hit!`);
+  res.status(200).send(teaData);
 });
 
 const port = 3000;
